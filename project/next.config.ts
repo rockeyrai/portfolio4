01aaf40e288ea50a1e10bb4ec1c7e5e5bd9b25a2
Rockey C.R <rockeyrai234@gmail.com>
2025-02-26 20:21:44 +0545

    second try at vercel hosting

--- project/next.config.ts
+++ project/next.config.ts
@@ -4,4 +4,5 @@ module.exports = {
   output: 'export',
   assetPrefix: isProd ? '/portfolio4/' : '',
   basePath: isProd ? '/portfolio4' : '',
+  trailingSlash: true, // Ensure trailing slashes for static hosting
 };
